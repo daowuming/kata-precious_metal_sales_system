921a939cdc7100b3d866bed0f2351d3507f45520
cuilihua1984@163.com <cuilihua1984@163.com>
2019-07-02 16:27:40 +0800

    添加会员信息和贵金属信息实体类

---
+++ src/main/java/com/coding/sales/enity/CustomerModels.java
@@ -0,0 +1,40 @@
+package com.coding.sales.enity;
+
+public class CustomerModels {
+    String memberId;
+    String memberName;
+    String memberType;
+    String memberNo;
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(String memberType) {
+        this.memberType = memberType;
+    }
+
+    public String getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = memberNo;
+    }
+}
---
+++ src/main/java/com/coding/sales/enity/MetalModels.java
@@ -0,0 +1,67 @@
+package com.coding.sales.enity;
+
+public class MetalModels {
+    String productName;
+    String productNo;
+    String product;
+    String unit;
+    String productPrice;
+    String discountCards;
+    String fullReduction;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductNo() {
+        return productNo;
+    }
+
+    public void setProductNo(String productNo) {
+        this.productNo = productNo;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getDiscountCards() {
+        return discountCards;
+    }
+
+    public void setDiscountCards(String discountCards) {
+        this.discountCards = discountCards;
+    }
+
+    public String getFullReduction() {
+        return fullReduction;
+    }
+
+    public void setFullReduction(String fullReduction) {
+        this.fullReduction = fullReduction;
+    }
+}
